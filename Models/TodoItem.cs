565d091e77a7f4e4c43cb08ba707852535d1bd20
Annie Vo <annievo.av@hotmail.com>
2019-04-16 18:38:31 -0500

    GET setup

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace PollutionApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; } // unique key in a relational database
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
